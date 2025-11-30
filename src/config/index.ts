2ee31076903dcc651bd192507b1ad20782a17e8b
sebastiankz <sebastianolivo@uninorte.edu.co>
2025-11-30 00:37:09 -0500

    Add fallback values for environment variables and update GitHub Actions workflows

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,8 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://roble-api.openlab.uninorte.edu.co';
 
 const config = {
-    API_AUTHENTICATION_URL: `${API_BASE_URL}/auth/${import.meta.env.VITE_PROJECT_ID}`,
-    API_DATABASE_URL: `${API_BASE_URL}/database/${import.meta.env.VITE_PROJECT_ID}`,
+    API_AUTHENTICATION_URL: `${API_BASE_URL}/auth/${import.meta.env.VITE_PROJECT_ID || 'prueba_2c17ca514c'}`,
+    API_DATABASE_URL: `${API_BASE_URL}/database/${import.meta.env.VITE_PROJECT_ID || 'prueba_2c17ca514c'}`,
 }
 
 export default config;
